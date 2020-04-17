1a67ab1b402da5bd9baee3040c39655b148e4ae3
rushi444 <rushi44@gmail.com>
2020-04-17 11:12:37 -0700

    typedefs and resolver

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { users, movies, reviews } = require('./sampleData');
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const typeDefs = `
+    type Query {
+        movie: String!
+        user: String!
+    }
+`;
+
+const resolvers = {
+  Query: {
+    movie() {
+      return 'The Lego Movie';
+    },
+    user() {
+      return 'Rushi';
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+  context: {
+    users,
+    movies,
+    reviews,
+  },
+});
 
 server.start(() => console.log('Server is running on localhost:4000'));
